9a1ae38d08908c9363be9a70cad21f9d38446cbd
Peter Jang <peter.x.jang@gmail.com>
2016-12-19 20:09:09 -0600

    Add angular with ng-model and ng-repeat examples

---
+++ app/assets/javascripts/stuff_ctrl.js
@@ -0,0 +1,13 @@
+/* global angular */
+
+(function() {
+  "use strict";
+
+  angular.module("app").controller("stuffCtrl", function($scope) {
+    $scope.myMessage = "Yo yo yo!";
+    $scope.myName = "Peter";
+    $scope.sum = 343 + 5353;
+    $scope.reviews = ["Hello", "goodbye", "fdsafsdadsfa"];
+  });
+
+}());
