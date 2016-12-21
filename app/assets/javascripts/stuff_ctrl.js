9d33b279c60e93a629d4a8f531a57e3678885f38
Peter Jang <peter.x.jang@gmail.com>
2016-12-20 18:21:01 -0600

    Add review filter using ng-if

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -7,10 +7,14 @@
     $scope.myMessage = "Yo yo yo!";
     $scope.myName = "Peter";
     $scope.sum = 343 + 5353;
-    $scope.reviews = ["Hello", "goodbye", "fdsafsdadsfa"];
+    $scope.reviews = ["Angular is cool!", "Angular is bad!", "Angular is all right all right all riiight"];
 
     $scope.addReview = function(inputText) {
       $scope.reviews.push(inputText);
+    };
+
+    $scope.isPositive = function(inputReviewText) {
+      return (inputReviewText.indexOf("bad") === -1);
     };
   });
 
