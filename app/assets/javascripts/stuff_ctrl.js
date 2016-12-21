8ad6d60addbb4747927476f51a71dc16370c87aa
Peter Jang <peter.x.jang@gmail.com>
2016-12-20 18:24:04 -0600

    Get rid of extra stuff

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -4,9 +4,6 @@
   "use strict";
 
   angular.module("app").controller("stuffCtrl", function($scope) {
-    $scope.myMessage = "Yo yo yo!";
-    $scope.myName = "Peter";
-    $scope.sum = 343 + 5353;
     $scope.reviews = ["Angular is cool!", "Angular is bad!", "Angular is all right all right all riiight"];
 
     $scope.addReview = function(inputText) {
