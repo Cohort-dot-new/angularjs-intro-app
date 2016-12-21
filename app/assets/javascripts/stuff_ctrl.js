e3a5018630c7f731d3a7b9fc2d1071f8060322a9
Peter Jang <peter.x.jang@gmail.com>
2016-12-20 18:39:43 -0600

    Convert array of strings to array of objects

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -4,14 +4,19 @@
   "use strict";
 
   angular.module("app").controller("stuffCtrl", function($scope) {
-    $scope.reviews = ["Angular is cool!", "Angular is bad!", "Angular is all right all right all riiight"];
+    $scope.reviews = [
+      {text: "Angular is cool!", rating: 5, reviewer: "Burton Wax"},
+      {text: "Angular is bad!", rating: 2, reviewer: "Emily Post"},
+      {text: "Angular is all right all right all riiight!", rating: 4, reviewer: "Matthew McConoghaauy"}
+    ];
 
-    $scope.addReview = function(inputText) {
-      $scope.reviews.push(inputText);
+    $scope.addReview = function(inputText, inputRating, inputName) {
+      var newReview = {text: inputText, rating: inputRating, reviewer: inputName};
+      $scope.reviews.push(newReview);
     };
 
-    $scope.isPositive = function(inputReviewText) {
-      return (inputReviewText.indexOf("bad") === -1);
+    $scope.isPositive = function(inputReview) {
+      return (inputReview.text.indexOf("bad") === -1);
     };
   });
 
